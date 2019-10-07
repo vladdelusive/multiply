a08568d5cd7df06e4d3c328436009905457f0035
Vlad Tovstochub <mr.tovstochub@mail.ru>
2019-10-07 23:32:45 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let x = BigInt(first);
+  let y = BigInt(second);
+  let answer =  (x * y);
+  return answer.toString();
 }
